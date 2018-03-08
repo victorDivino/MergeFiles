cdf0d96c2cae4d6a30d7f0bac48fa8468dfec8d4
Victor do Amor Divino <victordoamordivino@gmail.com>
2018-03-07 21:36:39 -0300

    Commit initial

--- MergeFiles/Program.cs
+++ MergeFiles/Program.cs
@@ -1,15 +1,55 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MergeFiles
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
+            string searchPath;
+            string fileOutput;
+
+            if (args.Length == 2)
+            {
+                searchPath = args[0];
+                fileOutput = args[1];
+            }
+            else
+            {
+                Console.WriteLine("Enter search path:");
+                searchPath = Console.ReadLine();
+                Console.WriteLine("Enter output file path:");
+                fileOutput = Console.ReadLine();
+            }
+
+            var enumerable = Directory.EnumerateFiles(searchPath, "*.txt", SearchOption.AllDirectories)
+                                      .SelectMany(File.ReadLines)
+                                      .Select(s => s.Trim())
+                                      .Where(s => !string.IsNullOrWhiteSpace(s))
+                                      .Distinct()
+                                      .OrderBy(x => x)
+                                      .ToArray();
+
+            var total = enumerable.Length;
+
+            Kurukuru.Spinner.Start("Start", spinner =>
+            {
+                var cont = 0.0;
+                var str = new StreamWriter(fileOutput);
+
+                foreach (var s in enumerable)
+                {
+                    spinner.Text = ($"{++cont / total:P}");
+                    str.WriteLine(s);
+                }
+            });
+
+            Console.WriteLine("Hit ENTER to open output file.");
+            Console.ReadLine();
+            Process.Start(fileOutput);
         }
     }
 }

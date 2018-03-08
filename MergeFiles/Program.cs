fbac5954a6eefb6d68fcca80bb3706721a51f2c1
Victor do Amor Divino <victordoamordivino@gmail.com>
2018-03-07 21:36:39 -0300

    Commit initial

--- MergeFiles/Program.cs
+++ MergeFiles/Program.cs
@@ -1,15 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MergeFiles
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
+            var searchPath = args[0];
+            var fileOutput = args[1];
+
+            if (File.Exists(fileOutput))
+                File.Delete(fileOutput);
+
+            Console.WriteLine("Searching files");
+
+            var files = Directory.EnumerateFiles(searchPath, "*.txt", SearchOption.AllDirectories)
+                .Select(fn => new FileInfo(fn)).Where(fi => fi.Length > 0);
+
+            Console.WriteLine("Start Merge");
+
+            foreach (var file in files)
+            {
+                Console.WriteLine($"Merged: {file.FullName}");
+                File.AppendAllLines(fileOutput, File.ReadLines(file.FullName));
+            }
+
+            Console.WriteLine("Finished");
+            Console.WriteLine("Hit ENTER to open output file.");
+            Console.ReadLine();
+            Process.Start(fileOutput);
         }
     }
 }
